75f74ed7bd44aa70f51cd23773801909502cb9ca
Jacek Dogiel <jacek.dogiel@gmail.com>
2018-12-10 11:14:14 +0100

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.strButton = new System.Windows.Forms.Button();
+            this.results = new System.Windows.Forms.RichTextBox();
+            this.partNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // strButton
+            // 
+            this.strButton.Location = new System.Drawing.Point(12, 12);
+            this.strButton.Name = "strButton";
+            this.strButton.Size = new System.Drawing.Size(75, 23);
+            this.strButton.TabIndex = 0;
+            this.strButton.Text = "Szukaj";
+            this.strButton.UseVisualStyleBackColor = true;
+            this.strButton.Click += new System.EventHandler(this.strButton_Click);
+            // 
+            // results
+            // 
+            this.results.Location = new System.Drawing.Point(94, 54);
+            this.results.Name = "results";
+            this.results.Size = new System.Drawing.Size(694, 384);
+            this.results.TabIndex = 1;
+            this.results.Text = "";
+            // 
+            // partNumber
+            // 
+            this.partNumber.Location = new System.Drawing.Point(94, 14);
+            this.partNumber.Name = "partNumber";
+            this.partNumber.Size = new System.Drawing.Size(262, 20);
+            this.partNumber.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.partNumber);
+            this.Controls.Add(this.results);
+            this.Controls.Add(this.strButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button strButton;
+        private System.Windows.Forms.RichTextBox results;
+        private System.Windows.Forms.TextBox partNumber;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Threading;
+using System.Net;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string part;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void strButton_Click(object sender, EventArgs e)
+        {
+            //nowy watek zeby nie blokowac interfejsu
+            Thread t = new Thread(()=>Start());
+            t.Start();
+            part = partNumber.Text;
+            results.Text = "";
+        }
+        void Start()
+        {
+            string url = "https://allegro.pl/listing?string=" + part + "&order=p&bmatch=cl-n-eng-global-uni-1-3-1130";
+            string source = getSource(url);
+            if (source == "") return;
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(source);
+            HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes("//span[@class='ecb7eff'] | //h2[@class='_4462670  ']");
+            
+            foreach (HtmlNode node in nodes)
+            {
+                this.Invoke(new MethodInvoker(delegate
+                {
+                    results.Text = results.Text + node.InnerText + Environment.NewLine;
+                }
+               ));
+            }
+        }
+
+        string getSource(string url)
+        {
+            try
+            {
+                WebClient mywebClient = new WebClient();
+                mywebClient.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+                byte[] myDataBuffer = mywebClient.DownloadData(url);
+                string source = Encoding.ASCII.GetString(myDataBuffer);
+
+                return source;
+            }
+            catch(Exception ex)
+            {
+                return "";
+            }
+            return "";
+        }
+    }
+}
